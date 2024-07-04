80805e7beab9aa66bd2b72c477144237bdf27c21
Ibsham <progwithibi@gmail.com>
2024-07-04 15:25:54 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// es6 imports ye new hain
+const express = require("express"); // commonjs modules
+
+const app = express();
+/**
+ * app meri express ap hai jispe main function aur routes handle kr skta hu
+ *
+ */
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+// Route Handler:
+// Port pe request ayi "/home"
+
+app.get("/", (req, res) => {
+  res.send({ hi: "Hello There" });
+});
